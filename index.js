581676af406a2ec0898a06e857e546a7bc1f2347
GusonYang <375339508@qq.com>
2016-02-05 13:45:56 +0800

    no message

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ _.assign(API, require('./lib/api_department'));
 _.assign(API, require('./lib/api_user'));
 _.assign(API, require('./lib/api_message'));
 _.assign(API, require('./lib/api_media'));
+_.assign(API, require('./lib/api_microapp'));
 
 module.exports = API;
--- test/api_media.js
+++ test/api_media.js
@@ -6,9 +6,24 @@ Api.config = {
 }
 var api = require("../lib/api_common");
 
+var media_id = '@lALOB54u8c0CAM0CAA';
+
 describe('media', function () {
     it('upload', function (done) {
         return api.media_upload(path.join(__dirname, './media/notify.png')).then(function (result) {
+            console.log(result);
+        }).then(done, done);
+    });
+});
+
+describe('microapp', function () {
+    it('create', function (done) {
+        return api.microapp_create({
+            appIcon: media_id,
+            appName: '测试微应用',
+            appDesc: '测试使用的微应用',
+            homepageUrl: 'http://oa.dingtalk.com/?h5'
+        }).then(function (result) {
             console.log(result);
         }).then(done, done);
     });
